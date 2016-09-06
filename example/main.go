e4e5fa895a16500ab2d820662d0a78e92bd88e8e
Matthew Di Ferrante <work@mattdf.com>
2016-09-06 14:50:35 +0100

    update example to use choose Trezor endpoint based on interface number

--- example/main.go
+++ example/main.go
@@ -32,12 +32,9 @@ func main() {
 		// 0x534c : 21324 vendor
 		// 0x0001 : 1     product
 		// 0x00   : Main Trezor Interface
-		// 0x01   : Main Trezor Endpoint
-		if info.Vendor == 21324 && info.Product == 1 {
+		if info.Vendor == 21324 && info.Product == 1 && info.Interface == 0 {
 			numberDevices++
-			device.SetInterface(0x00)
 			client.SetTransport(device)
-			device.SetEndpoint(0x01)
 		}
 
 	})
